4a690b4416462051844e35b341504253bf33f022
Vishwesh Shukla <vishweshshukla20@gmail.com>
2024-01-12 22:01:17 +0530

    commiting changes

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,25 @@ import { ThreeForTwoRule, BulkDiscountRule } from "./rules";
 import { Checkout } from "./checkout";
 
 // function to parse command line arguments
-function parseArgs(args: string[]): string[] {
-  return args.slice(2); // remove first two args (node and filename)
+function parseArgsAndExecuteCommands(args: string[]): void {
+  if (args.includes("--print-items")) {
+    Checkout.printAvailableItems();
+  } else {
+    // set up pricing rules
+    const pricingRules: PricingRule[] = [
+      new ThreeForTwoRule(),
+      new BulkDiscountRule(),
+    ];
+
+    // initialize checkout with pricing rules
+    const co = new Checkout(pricingRules);
+
+    // scan items from command line arguments
+    args.forEach((sku) => co.scan(sku));
+
+    // print total
+    console.log(`Total expected: $${co.total()}`);
+  }
 }
 
-// set up pricing rules
-const pricingRules: PricingRule[] = [
-  new ThreeForTwoRule(),
-  new BulkDiscountRule(),
-];
-
-// initialize checkout with pricing rules
-const co = new Checkout(pricingRules);
-
-// scan items
-const skus = parseArgs(process.argv);
-skus.forEach((sku) => co.scan(sku));
-
-// print total
-console.log(co.total());
+parseArgsAndExecuteCommands(process.argv.slice(2));
--- src/checkout.ts
+++ src/checkout.ts
@@ -4,9 +4,23 @@ export class Checkout {
   private items: Map<string, number>;
   private pricingRules: PricingRule[];
 
+  private static catalog = {
+    ipd: { name: "Super iPad", price: 549.99 },
+    mbp: { name: "MacBook Pro", price: 1399.99 },
+    atv: { name: "Apple TV", price: 109.5 },
+    vga: { name: "VGA adapter", price: 30.0 },
+  };
+
   constructor(pricingRules: PricingRule[]) {
     this.items = new Map<string, number>();
     this.pricingRules = pricingRules;
+  }
+
+  static printAvailableItems(): void {
+    console.log("Available items for purchase:");
+    for (const [sku, details] of Object.entries(Checkout.catalog)) {
+      console.log(`${sku}: ${details.name} - $${details.price.toFixed(2)}`);
+    }
   }
 
   scan(item: string): void {
